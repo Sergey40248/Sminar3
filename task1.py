d508055f973fa6d891cbf47875accde01e6e1dc5
Sergey <Sergey40248@mail.ru>
2022-12-01 13:10:29 +0300

    task1_end

--- task1.py
+++ task1.py
@@ -3,16 +3,18 @@
 # стоящих на нечётной позиции.
 #Пример:
 #[2, 3, 5, 9, 3] ->  элементы 3 и 9, ответ: 12
+print('enter numbers separated by spaces: ')
+array = [int(value) for value in input(). split()]
 
-quantity = int(input('input quantity elements:  '))
-array = []
-for i in range(quantity):
-    array.append(int(input('enter of numbers:  ')))
+s = []   # для вывода как в примере
 sum_odd = 0
 for j in range(len(array)):
     if j % 2 != 0:
         sum_odd += array[j]
+        s += list(str(array[j]))
+    
     else:
         j +=1
-print(array)
-print(sum_odd)
+
+# для вывода как в примере, работает только на однозначных ?
+print(f'{array} -> элементы {" и ".join(s)} ответ {sum_odd}')
